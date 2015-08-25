61f7bad3138192e7c64351c0460085cc03ad1630
Wijnand van Deutekom <wijnand@vdeute.com>
2015-08-25 22:33:10 +0200

    Tweaked config

--- htpc/sonarr/startdocker.sh
+++ htpc/sonarr/startdocker.sh
@@ -3,10 +3,12 @@
 
 echo "Starting up Sonarr"
 
-sudo docker run -dit \
+sudo docker run -it \
 --restart always \
 --name ${sonarr_containername} \
 -p ${sonarr_ports} \
 -v ${mnt_series}:/volumes/media \
+-v ${sonarr_datadir}:/volumes/config \
 -v ${sab_downloaddir}:/volumes/completed \
+--link ${sab_containername}:sabnzbd \
 wvdeutekom/sonarr:latest
